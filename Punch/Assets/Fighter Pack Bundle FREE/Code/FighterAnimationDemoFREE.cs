63c0eb714c8147d4ff08f08e3e74a8f52a9248f2
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-09-09 01:20:11 +0900

    【機能追加】パンチアニメーション、壁破壊、ひび画像

---
+++ Punch/Assets/Fighter Pack Bundle FREE/Code/FighterAnimationDemoFREE.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FighterAnimationDemoFREE : MonoBehaviour {
+	
+	public Animator animator;
+	public GameObject wall;
+
+	private Transform defaultCamTransform;
+	private Vector3 resetPos;
+	private Quaternion resetRot;
+	private GameObject cam;
+	private GameObject fighter;
+	private Rigidbody rigidbody;
+	private Transform wTransform;
+
+	void Start()
+	{
+		cam = GameObject.FindWithTag("MainCamera");
+		defaultCamTransform = cam.transform;
+		resetPos = defaultCamTransform.position;
+		resetRot = defaultCamTransform.rotation;
+		fighter = GameObject.FindWithTag("Player");
+		fighter.transform.position = new Vector3(0,0,0);
+		wTransform = wall.transform;
+	}
+
+	void OnGUI () 
+	{
+		if (GUI.RepeatButton (new Rect (815, 535, 100, 30), "Reset Scene")) 
+		{
+			defaultCamTransform.position = resetPos;
+			defaultCamTransform.rotation = resetRot;
+			fighter.transform.position = new Vector3(0,0,0);
+			animator.Play("Idle");
+		}
+
+		/*
+		if (GUI.RepeatButton (new Rect (25, 20, 100, 30), "Walk Forward")) 
+		{
+			animator.SetBool("Walk Forward", true);
+		}
+		else
+		{
+			animator.SetBool("Walk Forward", false);
+		}
+
+		if (GUI.RepeatButton (new Rect (25, 50, 100, 30), "Walk Backward")) 
+		{
+			animator.SetBool("Walk Backward", true);
+		}
+		else
+		{
+			animator.SetBool("Walk Backward", false);
+		}
+		*/
+
+		if (GUI.Button (new Rect (25, 20, 100, 30), "Punch")) 
+		{
+			animator.SetTrigger("PunchTrigger");
+
+
+			//DelayMethodを3.5秒後に呼び出す
+			Invoke("DestroyMethod", 1.0f);
+		
+		}
+	}
+
+	void DestroyMethod()
+	{
+		for (int i = 0; i < wTransform.childCount; i++) {
+
+			Vector3 dir;
+			dir = new Vector3 (
+				Random.Range (-1f, 1f),
+				Random.Range (-1f, 1f),
+				Random.Range (-1f, 1f));
+			rigidbody = wall.transform.GetChild (i).gameObject.GetComponent<Rigidbody> ();
+			rigidbody.AddForce (dir * 20, ForceMode.Impulse);
+			rigidbody.velocity += (dir * 20) / rigidbody.mass;
+
+		}
+	}
+}
---
+++ Punch/Assets/Script/DestroyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour {
+
+	//メンバ変数
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
